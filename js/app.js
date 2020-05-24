a479439597654de56dd1010ebfb34f860cbf07aa
AlanSalas <dubekone@gmail.com>
2020-05-23 21:40:51 -0500

    listPokemons

--- js/app.js
+++ js/app.js
@@ -1,12 +1,38 @@
-const modalPokemon = document.querySelector(".modalPokemon");
-const a = document.querySelector("#hola");
-const closeModal = document.querySelector(".closeModal");
+const pokemonCharacters = document.querySelector(".pokemonCharacters");
+const leftBtn = document.querySelector("#left");
+const rightBtn = document.querySelector("#right");
+let pagination = 0;
 
-a.addEventListener("click", function (e) {
-  e.preventDefault();
-  modalPokemon.classList.add("modalPokemonActive");
-});
+const loadPokemons = () => {
+  const promises = [];
+  for (let i = 1; i <= pagination + 12; i++) {
+    const urlApi = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    promises.push(fetch(urlApi).then((res) => res.json()));
+  }
+  Promise.all(promises).then((done) => {
+    const Pokemons = done.map((data) => ({
+      name: data.name,
+      image: data.sprites["front_default"],
+    }));
+    listPokemons(Pokemons);
+  });
+};
 
-closeModal.addEventListener("click", function () {
-  modalPokemon.classList.remove("modalPokemonActive");
-});
+const listPokemons = (pokemons) => {
+  const stringHTML = pokemons
+    .map((pokemon) => {
+      return `
+      <li class="pokemon">
+        <img src="${pokemon.image}" alt="${pokemon.name}" />
+        <a href="">${pokemon.name}</a>
+      </li>`;
+    })
+    .join("");
+  pokemonCharacters.innerHTML = stringHTML;
+};
+
+rightBtn.addEventListener("click", () => {});
+
+leftBtn.addEventListener("click", () => {});
+
+loadPokemons();
