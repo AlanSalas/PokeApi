1d7a6e0b5a96ad365a0f6e5b42552b8e0fe34d52
AlanSalas <dubekone@gmail.com>
2020-05-24 20:13:51 -0500

    filter Pokemons

--- js/app.js
+++ js/app.js
@@ -1,20 +1,32 @@
+const searchBox = document.querySelector("#search");
 const pokemonCharacters = document.querySelector(".pokemonCharacters");
 const leftBtn = document.querySelector("#left");
 const rightBtn = document.querySelector("#right");
+let arrayPokemons = [];
 let pagination = 0;
+
+searchBox.addEventListener("keyup", (e) => {
+  const stringSearch = e.target.value.toUpperCase();
+  const filteredPokemons = arrayPokemons.filter((pokemon) => {
+    return pokemon.name.toUpperCase().includes(stringSearch);
+  });
+  listPokemons(filteredPokemons);
+});
 
 const loadPokemons = () => {
   const promises = [];
-  for (let i = 1; i <= pagination + 12; i++) {
+  for (let i = 1; i <= 100; i++) {
     const urlApi = `https://pokeapi.co/api/v2/pokemon/${i}`;
     promises.push(fetch(urlApi).then((res) => res.json()));
   }
   Promise.all(promises).then((done) => {
     const Pokemons = done.map((data) => ({
+      id: data.id,
       name: data.name,
       image: data.sprites["front_default"],
     }));
     listPokemons(Pokemons);
+    arrayPokemons = Pokemons;
   });
 };
 
@@ -24,7 +36,7 @@ const listPokemons = (pokemons) => {
       return `
       <li class="pokemon">
         <img src="${pokemon.image}" alt="${pokemon.name}" />
-        <a href="">${pokemon.name}</a>
+        <a href="" id="${pokemon.id}">${pokemon.name}</a>
       </li>`;
     })
     .join("");
